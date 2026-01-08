9115d7c79cb33215f0a6cda05cf047a7b6b48219
shaikmohammedshaik653-hue <shaikmohammedshaik653@gmail.com>
2026-01-08 16:15:23 +0530

    Fix indentation for last_vol assignment

--- rsi_alert.py
+++ rsi_alert.py
@@ -221,7 +221,7 @@ def scan_and_alert():
         try:
             last_rsi = float(df.at[last_idx, "RSI"])
             last_close = float(df.at[last_idx, "Close"])
-            last_vol = float(df.at[last_idx, "Volume"))
+            last_vol = float(df.at[last_idx, "Volume"])
             vol_avg = float(df.at[last_idx, "VOL_AVG"))
             macd_val = float(df.at[last_idx, "MACD"))
             macd_sig = float(df.at[last_idx, "MACD_SIGNAL"))
